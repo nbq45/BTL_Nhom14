b431f8de9e6a6fdf0c5a6dbbb19dc36b8abf582f
DatVu555 <94444735+DatVu555@users.noreply.github.com>
2022-01-05 13:28:52 +0700

    UP 05/01 01:28

--- assets/js/food_detail.js
+++ assets/js/food_detail.js
@@ -1,87 +1,244 @@
-var check = false;
-
-function changeVal(el) {
-    var qt = parseFloat(el.parent().children(".qt").html());
-    var price = parseFloat(el.parent().children(".price").html());
-    var eq = Math.round(price * qt * 100) / 100;
-
-    el.parent().children(".full-price").html(eq + "€");
-
-    changeTotal();
+let btn = document.querySelectorAll('.btnAdding');
+let products = [{
+        name: 'Bạc xỉu',
+        tag: 'Bạc xỉu',
+        price: 35,
+        inCart: 0
+    },
+    {
+        name: 'Cà phê đen',
+        tag: 'Cà phê đen',
+        price: 28,
+        inCart: 0
+    },
+    {
+        name: 'Cà phê nâu',
+        tag: 'Cà phê nâu',
+        price: 30,
+        inCart: 0
+    },
+    {
+        name: 'Capuchino',
+        tag: 'Capuchino',
+        price: 40,
+        inCart: 0
+    },
+    {
+        name: 'Trà đào cam sả',
+        tag: 'Trà đào cam sả',
+        price: 35,
+        inCart: 0
+    },
+    {
+        name: 'Trà chanh',
+        tag: 'Trà chanh',
+        price: 28,
+        inCart: 0
+    },
+    {
+        name: 'Hồng trà sữa',
+        tag: 'Hồng trà sữa',
+        price: 40,
+        inCart: 0
+    },
+    {
+        name: 'Sữa chua đánh đá',
+        tag: 'Sữa chua đánh đá',
+        price: 28,
+        inCart: 0
+    },
+    {
+        name: 'Sữa chua cacao',
+        tag: 'Sữa chua cacao',
+        price: 30,
+        inCart: 0
+    },
+    {
+        name: 'Sữa chua cafe',
+        tag: 'Sữa chua cafe',
+        price: 35,
+        inCart: 0
+    },
+    {
+        name: 'Bánh quy bơ',
+        tag: 'Bánh quy bơ',
+        price: 28,
+        inCart: 0
+    },
+    {
+        name: 'Trà chanh',
+        tag: 'Trà chanh',
+        price: 20,
+        inCart: 0
+    },
+    {
+        name: 'Bánh quy socola',
+        tag: 'Bánh quy socola',
+        price: 30,
+        inCart: 0
+    },
+    {
+        name: 'Hướng dương',
+        tag: 'Hướng dương',
+        price: 20,
+        inCart: 0
+    },
+]
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+        totalCost(products[i]);
+        deleteCard()
+    })
 }
 
-function changeTotal() {
-
-    var price = 0;
-
-    $(".full-price").each(function(index) {
-        price += parseFloat($(".full-price").eq(index).html());
-    });
-
-    price = Math.round(price * 100) / 100;
-    var tax = Math.round(price * 0.05 * 100) / 100
-    var shipping = parseFloat($(".shipping span").html());
-    var fullPrice = Math.round((price + tax + shipping) * 100) / 100;
-
-    if (price == 0) {
-        fullPrice = 0;
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    if (productNumbers) {
+        document.querySelector('.bill-restaurant .number-order').textContent = productNumbers;
     }
-
-    $(".subtotal span").html(price);
-    $(".tax span").html(tax);
-    $(".total span").html(fullPrice);
+    if (productNumbers) {
+        document.querySelector('.order-card-user .number-order').textContent = productNumbers;
+    }
+    if (productNumbers) {
+        document.querySelector('.clearfix .number-order').textContent = productNumbers;
+    }
+    if (productNumbers) {
+        document.querySelector('.info-order .total').textContent = productNumbers;
+    }
 }
 
-$(document).ready(function() {
+function cartNumbers(product) {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.bill-restaurant .number-order').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.bill-restaurant .number-order').textContent = 1;
+    }
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.order-card-user .number-order').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.order-card-user .number-order').textContent = 1;
+    }
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.clearfix .number-order').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.clearfix .number-order').textContent = 1;
+    }
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.info-order .total').textContent = productNumbers + 1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.info-order .total').textContent = 1;
+    }
+    setItems(product);
+}
 
-    $(".remove").click(function() {
-        var el = $(this);
-        el.parent().parent().addClass("removed");
-        window.setTimeout(
-            function() {
-                el.parent().parent().slideUp('fast', function() {
-                    el.parent().parent().remove();
-                    if ($(".product").length == 0) {
-                        if (check) {
-                            $("#cart").html("<h1>The shop does not function, yet!</h1><p>If you liked my shopping cart, please take a second and heart this Pen on <a href='https://codepen.io/ziga-miklic/pen/xhpob'>CodePen</a>. Thank you!</p>");
-                        } else {
-                            $("#cart").html("<h1>No products!</h1>");
-                        }
-                    }
-                    changeTotal();
-                });
-            }, 200);
-    });
-
-    $(".qt-plus").click(function() {
-        $(this).parent().children(".qt").html(parseInt($(this).parent().children(".qt").html()) + 1);
-
-        $(this).parent().children(".full-price").addClass("added");
-
-        var el = $(this);
-        window.setTimeout(function() { el.parent().children(".full-price").removeClass("added");
-            changeVal(el); }, 150);
-    });
-
-    $(".qt-minus").click(function() {
-
-        child = $(this).parent().children(".qt");
-
-        if (parseInt(child.html()) > 1) {
-            child.html(parseInt(child.html()) - 1);
+function setItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    if (cartItems != null) {
+        if (cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
         }
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
 
-        $(this).parent().children(".full-price").addClass("minused");
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+    if (cartCost === null) {
+        localStorage.setItem('totalCost', product.price);
+    } else {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem('totalCost', cartCost + product.price);
+    }
+}
 
-        var el = $(this);
-        window.setTimeout(function() { el.parent().children(".full-price").removeClass("minused");
-            changeVal(el); }, 150);
-    });
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector('.order-card-groups');
+    let cartCost = localStorage.getItem('totalCost');
+    console.log(cartItems);
+    if (cartItems && productContainer) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="card-item"> 
+                <div class="clearfix">
+                    <button class="bi bi-plus"></button>
+                    <span class="number-order">${item.inCart}</span>
+                    <button class="bi bi-dash"></button>
+                    <span class="name-order">${item.name}</span>
+                </div>
+                <div class="note-order">
+                    <div class="row">
+                        <div class="col"><input type="text" id="textNote" placeholder="Thêm ghi chú..." style="border: none;"></div>
+                        <div class="col-auto"><span class="price-order">${item.inCart * item.price},000<sup>đ</sup></span></div>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+        let productPrice = document.querySelector('.price-total');
+        productPrice.innerHTML += `${cartCost},000<sup>đ</sup>`
+        let productTotalPrice = document.querySelector('.total-price .price-total');
+        productTotalPrice.innerHTML += `${cartCost},000<sup>đ</sup>`
+    }
+    let productOrder = document.querySelector('.order-list');
+    if (cartItems && productOrder) {
+        productOrder.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productOrder.innerHTML += `
+                <div class="row">
+                    <div class="col">
+                        <span class="order-item-number">${item.inCart}</span>
+                        <span class="name-order">${item.name}</span>
+                    </div>
+                    <div class="col-auto">
+                        <span class="price text-right">${item.inCart * item.price},000<sup>đ</sup></span>
+                    </div>
+                </div>
+            `;
+        });
+        let productPriceOrder = document.querySelector('.info-order .price-order');
+        productPriceOrder.innerHTML += `${cartCost},000<sup>đ</sup>`
+        let productFinalPrice = document.querySelector('.final-price-order .final-price');
+        productFinalPrice.innerHTML += `${cartCost},000<sup>đ</sup>`
+    }
+}
 
-    window.setTimeout(function() { $(".is-open").removeClass("is-open") }, 1200);
 
-    $(".btn").click(function() {
-        check = true;
-        $(".remove").click();
-    });
-});
+function deleteCard() {
+    var cardItem = document.querySelectorAll(".order-card-groups .card-item")
+    for (var i = 0; i < cardItem.length; i++) {
+        var productT = document.querySelectorAll(".bi-dash")
+        productT[i].addEventListener("click", function(event) {
+            var cartDelete = event.target
+            var cartitemR = cartDelete.parentElement
+            cartitemR.remove()
+            carttotal()
+                // console.log(cartitemR)
+        })
+    }
+}
+onLoadCartNumbers();
+displayCart();
--- food_detail.php
+++ food_detail.php
@@ -639,7 +639,7 @@
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     
-    <script src="assets/js/btnadding.js"></script>
+    <script src="assets/js/food_detail.js"></script>
 </body>
 
 </html>
