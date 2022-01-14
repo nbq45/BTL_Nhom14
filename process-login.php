825baa7ecf854e9304143b23c8b79d7367bd04de
nbq45 <1951060963@e.tlu.edu.vn>
2022-01-15 03:11:36 +0700

    process

--- process-login.php
+++ process-login.php
@@ -3,8 +3,8 @@
     session_start();
     // login.php TRUYỀN DỮ LIỆU SANG: NHẬN DỮ LIỆU TỪ admin.php gửi sang
     if(isset($_POST['btnSignIn'])){
-        $email = $_POST['txtEmail'];
-        $password = $_POST['txtPass'];
+        $email = trim($_POST['txtEmail']);
+        $password = trim($_POST['txtPass']);
 
         // Bước 01: Kết nối Database Server
         $conn = mysqli_connect('localhost','root','','shopee_food');
@@ -12,21 +12,32 @@
             die("Kết nối thất bại. Vui lòng kiểm tra lại các thông tin máy chủ");
         }
         // Bước 02: Thực hiện truy vấn
-        $sql = "SELECT * FROM khachhang WHERE email = '$email'AND password='$password'";
+        $sql = "SELECT * FROM khachhang WHERE email = '$email'";
 
         $result = mysqli_query($conn,$sql);
-
-        if(mysqli_num_rows($result) > 0){
+        $numRows = mysqli_num_rows($result);
+        echo $numRows;
+        if($numRows > 0){
             //cấp thẻ làm việc
-            $_SESSION['isLoginOk']=$email;
-            header("location: index.php");
+            $row = mysqli_fetch_assoc($result);
+            if(password_verify($password, $row['password'])) {
+                echo $numRows;
+                $_SESSION['isLoginOk'] = $email;
+                header("location: index.php");
+            }
+            else {
+                $error="mật khẩu của bạn chưa chính xác";
+                header("location: login.php?error=$error");
+                echo $numRows;
+            }
         }else{
             $error="Email hoặc mật khẩu của bạn chưa chính xác";
             header("location: login.php?error=$error");
+            echo $numRows;
         }
         //Bước 3: đóng kết nối
         mysqli_close($conn);
-    }else{
-        header("location:login.php");
-    }
+        }else{
+            header("location:login.php");
+        }
 ?>
--- process-register.php
+++ process-register.php
@@ -5,24 +5,11 @@ if(isset($_POST['btnRegister']))
     require __DIR__.'/../BTL_Nhom14/config/constants.php';
     //thực hiện truy vấn
     $result = mysqli_query($conn,"SELECT * FROM khachhang WHERE email='" . $_POST['email'] . "'");
-    $token = md5($_POST['email']).rand(10,9999);
-        //lưu lại thông tin đăng ký
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
-        $address = $_POST['address'];
-        $phone = $_POST['phone'];
-        $sql = "INSERT INTO khachhang(name, email, email_verification_link ,password, address, phone) VALUES('$name','$email','$token','$password','$address','$phone')";
-        mysqli_query($conn, $sql);
-        
-
-
-
 
     if(mysqli_num_rows($result) <= 0)//kiểm tra email chưa được dùng
     {
-        $token = md5($_POST['email']).rand(10,9999);
         //lưu lại thông tin đăng ký
+        $token = md5($_POST['email']).rand(10,9999);
         $name = $_POST['name'];
         $email = $_POST['email'];
         $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
@@ -32,7 +19,7 @@ if(isset($_POST['btnRegister']))
         mysqli_query($conn, $sql);
         //sau khi lưu xong, cần gửi tới mail đăng ký 1 đg link tới website
         //yêu cầu người dùng nhấp để kích hoạt; biến link sẽ gửi vào email
-        $link = "<a href='http://localhost/GitHub/BTL_NHOM14/verify-email.php?key=".$email."&token=".$token."'>Nhấp vào đây để kích hoạt</a>";
+        $link = "<a href='http://localhost/BTL_Nhom14/verify-email.php?key=".$email."&token=".$token."'>Nhấp vào đây để kích hoạt</a>";
         //quá trình gửi email
         include "send_mail.php";
         if(sendEmailForAccountActive($email,$link)){
@@ -44,6 +31,7 @@ if(isset($_POST['btnRegister']))
     else
     {
         echo "Bạn đã đăng ký thành công. Vui lòng xác nhận Email của bạn.";
+        echo $token;
     }
 }
 ?>
